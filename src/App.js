0a3774420641147fb6aa27ac0dbd3f1f6e340317
Jeff Rainey <rainey.jeff@gmail.com>
2022-02-15 12:33:40 -0500

    mobile tweaks on menu

--- src/App.js
+++ src/App.js
@@ -1104,19 +1104,21 @@ export default function App({ dataUrl }) {
         })}
       </div>
 
-      <div className="toggle-area">
-        <div id="toggleLegend" className={`${ showLegend ? 'open' : '' }` } onClick={toggleLegend}>
-          Show Legend <Caret />
-        </div>
-        <div id="toggleTimeline" className={`${ showTimeline ? 'open' : '' }`} onClick={toggleTimeline}>
-          Edit Time Range <Caret />
-        </div>
-        <div id="toggleShare" className={`${ showShare ? 'open' : '' }`} onClick={toggleShare}>
-          Share <Caret />
+      <div className="toggle-area-wrap">
+        <div className="toggle-area">
+          <div id="toggleLegend" className={`${ showLegend ? 'open' : '' }` } onClick={toggleLegend}>
+            Show Legend <Caret />
+          </div>
+          <div id="toggleTimeline" className={`${ showTimeline ? 'open' : '' }`} onClick={toggleTimeline}>
+            Edit Time Range <Caret />
+          </div>
+          <div id="toggleShare" className={`${ showShare ? 'open' : '' }`} onClick={toggleShare}>
+            Share <Caret />
+          </div>
         </div>
       </div>
       <div id="closeShare" onClick={toggleShare}>
-        X
+        <svg width="14px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" style={{'margin':'auto'}}><path fill="#fff" d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
       </div>
       <div className='sticky-container'>
         <aside className={
